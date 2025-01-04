a474d014fad8a0c61193e2329ee37cf90e672252
Doniyor Rufatov <Doniyor_Rufatov@epam.com>
2025-01-04 17:14:55 +0500

    --add impact analysis prompt and add some external events

--- impact_analysis/prompts.py
+++ impact_analysis/prompts.py
@@ -1,1 +1,47 @@
+PROMPT = """
+You are a helpful assistant.
+Your task is to help to analyze the negative impact of an external event to a company's business processes
 
+You will be given with the following data:
+ - A JSON structure of a company's business process as a graph representation
+ - An external event.
+
+1. The JSON has objects of process(nodes) and interactions of these objects(edges).
+2. The external event is a string that represents the event that might has a negative impact on the company's business processes.
+
+Your task is to analyze how objects and interactions between them are affected by the external event.
+For affection levels use the following values:
+- IMPACTED: The object's relationships or neighbour objects are directly affected and it might affect the object itself.
+- BLOCKED: The object is directly affected and it might affect the relationships or neighbour objects.
+
+By default all objects and interactions are AVAILABLE.
+
+Below is the DATA that you will be working with:
+
+COMPANY BUSINESS PROCESS:
+{company_business_process}
+
+EXTERNAL EVENT:
+{external_event}
+
+Your output should be strictly in the following output format:
+{output_format}
+"""  # noqa
+
+OUTPUT_FORMAT = """
+{
+  "objects": [
+    {
+      "name": "object_name",
+      "availability": "AVAILABLE | IMPACTED | BLOCKED"
+    }
+  ],
+  "interactions": [
+    {
+      "from": "object_name",
+      "to": "object_name",
+      "availability": "AVAILABLE | IMPACTED | BLOCKED"
+    }
+  ]
+}
+"""  # noqa
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+EVENTS = [
+    "Payment Gateway Downtime: Disruption in payment verification due to financial institution outages,"
+    " delaying reservations",
+    "Service Provider Strikes or Shortages: Inability of service providers to fulfill reservations due to"
+    " labor disputes or resource shortages",
+    "Cybersecurity Breaches: Compromised client data and transaction security from cyber attacks,"
+    " eroding trust and causing operational disruptions",
+    "Regulatory Changes: Necessary system updates and process modifications due to new or altered legal requirements,"
+    " causing delays",
+    "Natural Disasters or Pandemics: Service interruptions and high cancellation rates triggered by environmental or"
+    " health crises",
+]
+
+INTRODUCTION_MESSAGE = """
+Welcome to the Solution Architect Assistant!
+
+Here you can analyze the negative impact of an external events to your company's business process and generate high level solutions that help you to mitigate the impact.
+"""
+
+
 def main():
-    print("Hello World!")
+    print(INTRODUCTION_MESSAGE)
+    formatted_events = "\n\t".join([f"{i + 1}. {event}" for i, event in enumerate(EVENTS)])
+    print(f"To start please select an event from the list below:\n\t{formatted_events}")
+
+    while True:
+        try:
+            event_number = int(input("Enter the event number: ")) - 1
+            if 0 <= event_number < len(EVENTS):
+                break
+            else:
+                print("Invalid event number. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    print(f"You have selected the following event:\n\t{EVENTS[event_number]}")
+    print("Now we are starting Impact analysis process...")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
